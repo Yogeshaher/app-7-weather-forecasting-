05a478994519944c074f8be8a20b7679f0235be8
yogesh <ahersri@gmail.com>
2023-04-04 14:55:45 +0530

    get data in backend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import plotly.express as px
-
+from backend import get_data
 st.title("Weather Forecast For The Next Days")
 place = st.text_input("place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -9,13 +9,7 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = [""]
-    temperature = []
-    temperature = [days * i for i in temperature]
-    return dates, temperature
-
-d, t = get_data(days)
+d, t = get_data(place, days, option)
 
 figure = px.line(x=d, y=t, labels={"x": "date", "y": "temperature (c)"})
 st.plotly_chart(figure)
